7013d278dd1da7551299c2db14c0a29da0a1bd4c
elmot73 <elijah.mot@gmail.com>
2014-08-26 01:12:47 +0400

    HT IR seeker mode added

--- ev3-libusb/src/test/java/elmot/javabrick/ev3/libusb/MainHTIRSeeker.java
+++ ev3-libusb/src/test/java/elmot/javabrick/ev3/libusb/MainHTIRSeeker.java
@@ -2,6 +2,7 @@ package elmot.javabrick.ev3.libusb;
 
 import elmot.javabrick.ev3.EV3;
 import elmot.javabrick.ev3.EV3FactoryUsb;
+import elmot.javabrick.ev3.HTIRSeeker;
 import elmot.javabrick.ev3.PORT;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -27,7 +28,7 @@ public class MainHTIRSeeker {
     public void doTest() throws IOException, InterruptedException, UsbException {
         EV3 ev3 = findBrick();
         for (long startMs = System.currentTimeMillis(); System.currentTimeMillis() - startMs < 100000; ) {
-            ev3.HT_IR_SEEKER.setMode(0,PORT.P4,0);
+            ev3.HT_IR_SEEKER.setMode(0,PORT.P4, HTIRSeeker.MODE.DIR_AC);
             int data = ev3.HT_IR_SEEKER.read(0, PORT.P4);
             System.out.println("data = " + data);
 
--- ev3-net/src/test/java/elmot/javabrick/ev3/net/MainHTIRSeeker.java
+++ ev3-net/src/test/java/elmot/javabrick/ev3/net/MainHTIRSeeker.java
@@ -1,6 +1,7 @@
 package elmot.javabrick.ev3.net;
 
 import elmot.javabrick.ev3.EV3;
+import elmot.javabrick.ev3.HTIRSeeker;
 import elmot.javabrick.ev3.PORT;
 
 import java.io.IOException;
@@ -16,7 +17,7 @@ public class MainHTIRSeeker {
     @Test
     public void doTest() throws IOException, InterruptedException {
         EV3 ev3 = EV3Base.openBlock();
-        ev3.HT_IR_SEEKER.setMode(0,PORT.P4,0);
+        ev3.HT_IR_SEEKER.setMode(0,PORT.P4, HTIRSeeker.MODE.DIR_AC);
         for (long startMs = System.currentTimeMillis(); System.currentTimeMillis() - startMs < 10000; ) {
             int data = ev3.HT_IR_SEEKER.read(0, PORT.P4);
             System.out.println("data = " + data);
